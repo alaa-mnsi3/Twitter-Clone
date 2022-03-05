eebc1b775787aaebb8366968c51395bc0e55931b
alaa-mnsi3 <alaamnsy334@gmail.com>
2022-03-05 16:03:39 +0200

    push website

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App()
     <BrowserRouter>
       <div className="App">
         <iframe width="0px" title='music for twitter' height="0px" type="text/html" frameBorder='0' src="http://www.youtuberepeater.com/watch?v=7wtfhZwyrcc&name=Imagine+Dragons+Believer?autoplay=1#gsc.tab=0&origin=http://localhost:3000" allowFullScreen allow='acceleromete; autoplay; encrypted-media; gyroscope;'></iframe>
-         <Interface/>
-        {id &&
-        <Home/>
-        }
+        <Routes>
+          <Route  path='/:any' element={<Interface/>}/>
+        </Routes>
+        {id && <Home/>}
       </div>
     </BrowserRouter>  
   );
