221a25e55422b80ca8e6b96e0e265764490fc5c5
alaa-mnsi3 <alaamnsy334@gmail.com>
2022-03-22 14:25:06 +0200

    push website

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
-import store,{persistor} from './store'
-import {PersistGate} from "redux-persist/integration/react"
+import store from './store'
 import './i18n'
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userIdSlice from './Slices/getUserIdSlice'
 import IdReplySlice from './Slices/IdReplySlice';
-import { persistStore  , persistReducer} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'
 import {combineReducers} from 'redux';
 
-const persistConfig ={
-    key:'root',
-    storage,
-    whitelist :['userIdSlice']
-}
+
 const reducers = combineReducers({
     userIdSlice,
     IdReplySlice,
 });
-const persistedReducer = persistReducer(persistConfig,reducers)
 
-const store= configureStore({reducer:persistedReducer})
-export const persistor = persistStore(store)
+const store= configureStore({reducer:reducers})
 export default store;
